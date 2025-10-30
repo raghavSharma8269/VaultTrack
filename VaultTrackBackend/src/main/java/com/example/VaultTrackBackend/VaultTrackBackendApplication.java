06f66528dac58a8ccdb5fbbae963145e81093665
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-10-29 23:02:53 -0400

    add frontend and backend

---
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/VaultTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.VaultTrackBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultTrackBackendApplication.class, args);
+	}
+
+}
---
+++ VaultTrackBackend/src/test/java/com/example/VaultTrackBackend/VaultTrackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VaultTrackBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultTrackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
