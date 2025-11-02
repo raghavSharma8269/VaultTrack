533cd0f07d828ff7bc330108d41345ef60fe4469
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-01 23:40:56 -0400

    added controller for all auth services

---
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/AuthController.java
@@ -0,0 +1,46 @@
+package com.example.VaultTrackBackend.controller;
+
+import com.example.VaultTrackBackend.dto.auth.AuthResponseDTO;
+import com.example.VaultTrackBackend.dto.auth.LoginRequestDTO;
+import com.example.VaultTrackBackend.dto.auth.RegisterRequestDTO;
+import com.example.VaultTrackBackend.service.auth.LoginService;
+import com.example.VaultTrackBackend.service.auth.RegisterService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final LoginService loginService;
+    private final RegisterService registerService;
+
+    public AuthController(
+            LoginService loginService,
+            RegisterService registerService
+    ) {
+        this.loginService = loginService;
+        this.registerService = registerService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthResponseDTO> register (
+            @Valid
+            @RequestBody
+            RegisterRequestDTO registerRequestDTO
+    ) {
+        return registerService.execute(registerRequestDTO);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponseDTO> login (
+            @Valid
+            @RequestBody
+            LoginRequestDTO loginRequestDTO
+    ) {
+        return loginService.execute(loginRequestDTO);
+    }
+}
