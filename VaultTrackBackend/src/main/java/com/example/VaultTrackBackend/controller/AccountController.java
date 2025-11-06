18532e131cc366d03f8acbf07660318e17c1229d
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-06 17:38:37 -0500

    added GetAccountService endpoint

--- VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/AccountController.java
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/AccountController.java
@@ -1,13 +1,17 @@
 package com.example.VaultTrackBackend.controller;
 
 import com.example.VaultTrackBackend.dto.account.CreateAccountDTO;
+import com.example.VaultTrackBackend.dto.account.GetAccountResponseDTO;
 import com.example.VaultTrackBackend.dto.account.UpdateAccountDTO;
+import com.example.VaultTrackBackend.model.enums.AccountType;
 import com.example.VaultTrackBackend.service.account.CreateAccountService;
 import com.example.VaultTrackBackend.service.account.DeleteAccountService;
+import com.example.VaultTrackBackend.service.account.GetAccountService;
 import com.example.VaultTrackBackend.service.account.UpdateAccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -16,15 +20,18 @@ public class AccountController {
     private final CreateAccountService createAccountService;
     private final UpdateAccountService updateAccountService;
     private final DeleteAccountService deleteAccountService;
+    private final GetAccountService getAccountService;
 
     public AccountController(
             CreateAccountService createAccountService,
             UpdateAccountService updateAccountService,
-            DeleteAccountService deleteAccountService
+            DeleteAccountService deleteAccountService,
+            GetAccountService getAccountService
     ) {
         this.createAccountService = createAccountService;
         this.updateAccountService = updateAccountService;
         this.deleteAccountService = deleteAccountService;
+        this.getAccountService = getAccountService;
     }
 
     @PostMapping
@@ -46,5 +53,13 @@ public class AccountController {
             @PathVariable("accountId") UUID accountId
     ) {
         return deleteAccountService.execute(accountId);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetAccountResponseDTO>> getAccounts(
+            @RequestParam(required = false) String query,
+            @RequestParam(required = false) AccountType accountType
+    ) {
+        return getAccountService.getAccounts(query, accountType);
     }
 }
