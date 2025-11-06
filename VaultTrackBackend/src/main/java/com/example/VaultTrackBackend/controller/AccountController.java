340b26a48433d16ba218b815296f8aaa186640ff
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-06 17:16:25 -0500

    added DeleteAccountService endpoint

--- VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/AccountController.java
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/AccountController.java
@@ -3,22 +3,28 @@ package com.example.VaultTrackBackend.controller;
 import com.example.VaultTrackBackend.dto.account.CreateAccountDTO;
 import com.example.VaultTrackBackend.dto.account.UpdateAccountDTO;
 import com.example.VaultTrackBackend.service.account.CreateAccountService;
+import com.example.VaultTrackBackend.service.account.DeleteAccountService;
 import com.example.VaultTrackBackend.service.account.UpdateAccountService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/accounts")
 public class AccountController {
     private final CreateAccountService createAccountService;
     private final UpdateAccountService updateAccountService;
+    private final DeleteAccountService deleteAccountService;
 
     public AccountController(
             CreateAccountService createAccountService,
-            UpdateAccountService updateAccountService
+            UpdateAccountService updateAccountService,
+            DeleteAccountService deleteAccountService
     ) {
         this.createAccountService = createAccountService;
         this.updateAccountService = updateAccountService;
+        this.deleteAccountService = deleteAccountService;
     }
 
     @PostMapping
@@ -33,5 +39,12 @@ public class AccountController {
             @RequestBody UpdateAccountDTO updateAccountDTO
     ){
         return updateAccountService.execute(updateAccountDTO);
+    }
+
+    @DeleteMapping("/{accountId}")
+    public ResponseEntity<String> deleteAccount(
+            @PathVariable("accountId") UUID accountId
+    ) {
+        return deleteAccountService.execute(accountId);
     }
 }
