3bbbe3e4ba16c50b9eebd4d36ba14e7ea42b7f1a
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-10 16:47:51 -0500

    added logic for deleting transactios

--- VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/TransactionController.java
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/TransactionController.java
@@ -2,22 +2,25 @@ package com.example.VaultTrackBackend.controller;
 
 import com.example.VaultTrackBackend.dto.transaction.CreateTransactionDTO;
 import com.example.VaultTrackBackend.service.transaction.CreateTransactionService;
+import com.example.VaultTrackBackend.service.transaction.DeleteTransaction;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transactions")
 public class TransactionController {
 
     private final CreateTransactionService createTransactionService;
+    private final DeleteTransaction deleteTransactionService;
 
     public TransactionController(
-            CreateTransactionService createTransactionService
+            CreateTransactionService createTransactionService,
+            DeleteTransaction deleteTransactionService
     ) {
         this.createTransactionService = createTransactionService;
+        this.deleteTransactionService = deleteTransactionService;
     }
 
     @PostMapping
@@ -26,4 +29,12 @@ public class TransactionController {
     ) {
         return createTransactionService.execute(createTransactionDTO);
     }
+
+    @DeleteMapping("/{transactionId}")
+    public ResponseEntity<String> deleteTransaction(
+            @PathVariable("transactionId") UUID transactionId
+    ) {
+        return deleteTransactionService.deleteTransaction((transactionId));
+    }
+
 }
---
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/service/transaction/DeleteTransaction.java
@@ -0,0 +1,67 @@
+package com.example.VaultTrackBackend.service.transaction;
+
+import com.example.VaultTrackBackend.model.entity.Account;
+import com.example.VaultTrackBackend.model.entity.Transaction;
+import com.example.VaultTrackBackend.model.entity.User;
+import com.example.VaultTrackBackend.model.enums.TransactionType;
+import com.example.VaultTrackBackend.repository.AccountRepository;
+import com.example.VaultTrackBackend.repository.TransactionRepository;
+import com.example.VaultTrackBackend.service.auth.GetCurrentUserService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@Slf4j
+public class DeleteTransaction {
+
+    private final TransactionRepository transactionRepository;
+    private final AccountRepository accountRepository;
+    private final GetCurrentUserService getCurrentUserService;
+
+    public DeleteTransaction(
+            TransactionRepository transactionRepository,
+            AccountRepository accountRepository,
+            GetCurrentUserService getCurrentUserService
+    ) {
+        this.transactionRepository = transactionRepository;
+        this.accountRepository = accountRepository;
+        this.getCurrentUserService = getCurrentUserService;
+    }
+
+    public ResponseEntity<String> deleteTransaction(UUID transactionId) {
+
+        log.info("Deleting transaction with ID: {}", transactionId);
+        User user = getCurrentUserService.execute();
+
+        Transaction transaction = transactionRepository.findById(transactionId).orElse(null);
+
+        if (transaction == null) {
+            return ResponseEntity.badRequest().body("Invalid transaction ID");
+        }
+
+        Account account = transaction.getAccount();
+
+        if (account == null) {
+            return ResponseEntity.badRequest().body("Invalid account ID");
+        }
+        if (account.getUser() != user) {
+            return ResponseEntity.status(403).body("You do not have permission to delete this transaction");
+        }
+
+        if (transaction.getTransactionType().equals(TransactionType.INCOME)){
+            account.setCurrentBalance(account.getCurrentBalance().subtract(transaction.getAmount()));
+            log.info("Subtracted {}", transaction.getAmount());
+        }
+        else if (transaction.getTransactionType().equals(TransactionType.EXPENSE)){
+            account.setCurrentBalance(account.getCurrentBalance().add(transaction.getAmount()));
+            log.info("Added {}", transaction.getAmount());
+        }
+
+        transactionRepository.delete(transaction);
+        log.info("Transaction with ID: {} deleted successfully", transactionId);
+        return ResponseEntity.ok("Transaction deleted successfully");
+    }
+}
