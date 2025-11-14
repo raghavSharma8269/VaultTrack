f4bbb2ac9620f82a8bee00509a7a469a913a99b5
raghavSharma8269 <r.sharmali2023@gmail.com>
2025-11-13 21:33:44 -0500

    added logic to get transactions to transactions endpoint

--- VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/TransactionController.java
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/TransactionController.java
@@ -1,11 +1,18 @@
 package com.example.VaultTrackBackend.controller;
 
 import com.example.VaultTrackBackend.dto.transaction.CreateTransactionDTO;
+import com.example.VaultTrackBackend.dto.transaction.GetTransactionResponseDTO;
+import com.example.VaultTrackBackend.model.enums.TransactionCategory;
+import com.example.VaultTrackBackend.model.enums.TransactionType;
 import com.example.VaultTrackBackend.service.transaction.CreateTransactionService;
-import com.example.VaultTrackBackend.service.transaction.DeleteTransaction;
+import com.example.VaultTrackBackend.service.transaction.DeleteTransactionService;
+import com.example.VaultTrackBackend.service.transaction.GetTransactionService;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -13,14 +20,17 @@ import java.util.UUID;
 public class TransactionController {
 
     private final CreateTransactionService createTransactionService;
-    private final DeleteTransaction deleteTransactionService;
+    private final DeleteTransactionService deleteTransactionService;
+    private final GetTransactionService getTransactionService;
 
     public TransactionController(
             CreateTransactionService createTransactionService,
-            DeleteTransaction deleteTransactionService
+            DeleteTransactionService deleteTransactionService,
+            GetTransactionService getTransactionService
     ) {
         this.createTransactionService = createTransactionService;
         this.deleteTransactionService = deleteTransactionService;
+        this.getTransactionService = getTransactionService;
     }
 
     @PostMapping
@@ -35,6 +45,23 @@ public class TransactionController {
             @PathVariable("transactionId") UUID transactionId
     ) {
         return deleteTransactionService.deleteTransaction((transactionId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<GetTransactionResponseDTO>> getTransactions(
+            @RequestParam(required = false) String transactionName,
+            @RequestParam(required = false) TransactionType transactionType,
+            @RequestParam(required = false) TransactionCategory transactionCategory,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS") LocalDateTime start,
+            @RequestParam(required = false) @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss.SSSSSS") LocalDateTime end
+    ){
+        return getTransactionService.execute(
+                start,
+                end,
+                transactionCategory,
+                transactionType,
+                transactionName
+        );
     }
 
 }
