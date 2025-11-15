7bd6f3a35dcfd443efa7239c71febcd0fe2abb0e
Archayne <149523000+Archayne@users.noreply.github.com>
2025-11-15 02:57:33 -0500

    updated: BudgetController (WIP)

--- VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/BudgetController.java
+++ VaultTrackBackend/src/main/java/com/example/VaultTrackBackend/controller/BudgetController.java
@@ -1,17 +1,19 @@
 package com.example.VaultTrackBackend.controller;
 
 import com.example.VaultTrackBackend.dto.budget.BudgetDTO;
+import com.example.VaultTrackBackend.dto.budget.TransactionDTO;
 import com.example.VaultTrackBackend.model.entity.Budget;
+import com.example.VaultTrackBackend.model.entity.Transaction;
 import com.example.VaultTrackBackend.service.budget.BudgetService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.math.BigDecimal;
 import java.security.Principal;
+import java.time.YearMonth;
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -33,6 +35,22 @@ public class BudgetController {
                 .plannedAmount(b.getPlannedAmount())
                 .build();
         return ResponseEntity.status(HttpStatus.CREATED).body(out);
+    }
+
+    @PostMapping("/{id}/transactions")
+    public ResponseEntity<TransactionDTO> addTransaction(@PathVariable UUID id, @RequestBody TransactionDTO dto, Principal p){
+        UUID userId = ... //validate user owns budget id
+        dto.setBudgetId(id);
+        Transaction tx = budgetService.addTransaction(userId, dto);
+        TransactionDTO out = ... //map fields
+        return ResponseEntity.status(HttpStatus.CREATED).body(out);
+    }
+
+    @GetMapping("/{id}/summary")
+    public ResponseEntity <Map<String, BigDecimal>> summary(@PathVariable UUID id, @RequestParam(required = false)YearMonth month, Principal p){
+        if (month == null) month = YearMonth.now();
+        Map<String, BigDecimal> summary = budgetService.monthlySummary(id, month);
+        return ResponseEntity.ok(summary);
     }
 
 
